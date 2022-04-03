c94f9366d5e397c33f6a57279d5179330c858d5a
Asma <as1588ma@gmail.com>
2022-04-03 12:40:13 -0400

    completed my homework

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(drivers) {
+  const [firstDriver, secondDriver] = drivers;
+  return [firstDriver, secondDriver];
+}
+function returnLastTwoDrivers(drivers) {
+  //  return drivers.slice(drivers.length - 2);
+  const driversLength = drivers.length;
+  return [drivers[driversLength - 2], drivers[driversLength - 1]];
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(multiplier) {
+  return (num) => {
+    return multiplier * num;
+  };
+}
+function fareDoubler(num) {
+  return num * 2;
+}
+function fareTripler(num) {
+  return num * 3;
+}
+function selectDifferentDrivers(arrayOfDrivers, fn) {
+  return fn(arrayOfDrivers);
+}
